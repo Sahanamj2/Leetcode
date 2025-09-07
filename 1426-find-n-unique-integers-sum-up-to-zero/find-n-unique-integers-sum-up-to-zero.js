1d10c00586d51d819f6ec4aa65544f3997acd91e
Sahana M J <72748263+Sahanamj2@users.noreply.github.com>
2025-09-07 23:11:26 +0530

    Time: 1 ms (34.48%) | Memory: 57.9 MB (6.90%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.js
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.js
@@ -3,10 +3,19 @@
  * @return {number[]}
  */
 var sumZero = function(n) {
-    const res = [];
-  for (let i = 1; i <= Math.floor(n / 2); i++) {
-    res.push(i, -i);
+//     const res = [];
+//   for (let i = 1; i <= Math.floor(n / 2); i++) {
+//     res.push(i, -i);
+//   }
+//   if (n % 2 === 1) res.push(0);
+//   return res;
+  if (n === 1) return [0];
+  const res = [];
+  let sum = 0;
+  for (let i = 1; i < n; i++) {
+    res.push(i);
+    sum += i;
   }
-  if (n % 2 === 1) res.push(0);
+  res.push(-sum); // unique and ensures total sum = 0
   return res;
 };
