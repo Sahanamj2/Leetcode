c7a09e097ff8ade4ce9bb216d900f30537e7428f
Sahana M J <72748263+Sahanamj2@users.noreply.github.com>
2025-12-03 13:44:34 +0530

    Time: 1 ms (65.99%), Space: 55.8 MB (90.81%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    const n = nums.length;
+    const expectedSum = (n*(n+1))/2
+    const actualSum = nums.reduce((acc, curr) => acc+curr)
+    return expectedSum - actualSum;
+};
